59dc3f47bc42ec25e50c6ba6c4b52d4e5f035145
jenntang1 <1039@holbertonschool.com>
2019-12-11 18:11:57 +0000

    2. Updating to correct if statement to print comma and space for all combos that are not 89, jenntang1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for num in range(0, 99):
     if num % 10 > num / 10:
-        print("{:02d}, ".format(num), end="")
-        if num == 89:
-            print("{}".format(num), end="")
+        print("{:02d}".format(num), end="")
+        if num != 89:
+            print(end=", ")
 print()
