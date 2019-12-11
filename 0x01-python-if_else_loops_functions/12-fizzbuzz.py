d9219441f297af19b6a3ae96d5bd0b118ce676e9
jenntang1 <1039@holbertonschool.com>
2019-12-11 01:45:52 +0000

    1. Creating function that prints 1-100 with a Fizz Buzz twist, jenntang1

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif number % 3 == 0:
+            print("Fizz", end=" ")
+        elif number % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{}".format(number), end=" ")
+    if number != 100:
+        print(" ")
