0b0bf1ab97a1a1540ec8f7e34e38018b943dfb09
jenntang1 <1039@holbertonschool.com>
2019-12-11 01:49:36 +0000

    3. Updating to remove unnecessary specification in last print statement, jenntang1

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-""" loop through range and print up to 98, separated by comma and space """
 for numbers in range(00, 100):
     if numbers <= 98:
-        print("{:02d}".format(numbers), end=", ")
-
-""" if number is 99, print it and a new line """
+        print("{:02d}, ".format(numbers), end="")
 if numbers == 99:
-    print("{:02d}".format(numbers))
+    print("{:d}".format(numbers))
