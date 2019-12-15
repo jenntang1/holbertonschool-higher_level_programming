99ce42159d5383711c30cae7beb5eed3a28375a5
jenntang1 <1039@holbertonschool.com>
2019-12-15 20:50:11 +0000

    3. Updating to correct if statements after iterating to position for insertion, jenntang1

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -13,33 +13,41 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+	/* declare variable for new node */
 	listint_t *new;
-	listint_t *current;
-
+	/* declare variable to hold head */
+	listint_t *current = *head;
+	/* allocate memory for new node but if it fails, return NULL */
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-
+	/* create new node */
 	new->n = number;
-	current = *head;
-
-	if (head == NULL)
-		return (NULL);
-
+	new->next = NULL;
+	/* if it's a empty list, then, set new node as head */
 	if (*head == NULL)
-		new->next = NULL;
-		*head = new;
-		return (*head);
-
-	if (current->n > number)
-		new->next = current;
+	{
 		*head = new;
 		return (new);
-
-	while (current)
-		if (current->next)
-			if (current->next->n > number)
-				new->next = current->next;
-				current->next = new;
-				return (new);
+	}
+	/* if element given is greater than head, then, make new node head */
+	if ((*head)->n >= number)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	/**
+	 * 1. iterate through list to NULL and
+	 * 2. if element given is less than the node after it
+	 * 3. then, point new node to the node after it
+	 * 4. point the current node to the new node
+	 */
+	while ((current->next != NULL) && (current->next->n < number))
+	{
+		current = current->next;
+	}
+	new->next = current->next;
+	current->next = new;
+	return (new);
 }
