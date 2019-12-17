b2c47cd7cbd34f580376c996b214761732b45a59
jenntang1 <1039@holbertonschool.com>
2019-12-17 20:35:36 +0000

    2. Updating to account for empty list, jenntang1

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for current in my_list:
-        if idx < 0:
-            return my_list
-        elif idx > len(my_list):
-            return my_list
-        elif current == idx:
-            my_list[current] = element
-            return my_list
+    if my_list:
+        for current in my_list:
+            if idx < 0:
+                return my_list
+            elif idx > len(my_list):
+                return my_list
+            elif current == idx:
+                my_list[current] = element
+                return my_list
+    else:
+        return my_list
