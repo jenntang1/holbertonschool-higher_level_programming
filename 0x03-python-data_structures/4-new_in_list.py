4160de7d12c66a9cabd6d4007bd577e9fa0c267d
jenntang1 <1039@holbertonschool.com>
2019-12-17 21:51:59 +0000

    2. Updating to account for edge cases, jenntang1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    for current in my_list:
-        if idx < 0:
-            return my_list
-        elif idx > len(my_list):
-            return my_list
-        elif current == idx:
-            newer_list = my_list[:]
-            newer_list[current] = element
-            return newer_list
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    elif my_list:
+        newer_list = my_list[:]
+        newer_list[idx] = element
+        return newer_list
