c3aabe9d089ad828009674e42f9a370a5c1e73b8
jenntang1 <1039@holbertonschool.com>
2019-12-18 18:13:28 +0000

    2. Updating to correct print statement format, jenntang1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for element in range(len(matrix)):
-        for index in matrix[element]:
-            if element != len(matrix):
-                print("{:d} ".format(index))
+        for index in range(len(matrix[element])):
+            print("{:d}".format(matrix[element][index]), end="")
+            if (index != (len(matrix[element]) - 1)):
+                print(end=" ")
+        print("")
