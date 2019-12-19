ab619c10488f5ee3ae79f435ca73d461f7f35504
jenntang1 <1039@holbertonschool.com>
2019-12-19 03:52:09 +0000

    1. Creating function that checks if singly linked list is a palindrome, jenntang1

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_helper - function that returns a truth value
+ * @head: head of singly linked list
+ * @node: node that includes the palindrome to check for
+ * Description: 13. Linked list palindrome
+ * Return: see below
+ * 1. upon success, 0 if palindrome is found
+ * 2. upon fail, 1 if palindrome is not found
+ */
+int is_helper(listint_t **head, listint_t *node)
+{
+	int check1 = 0, check2 = 0;
+	listint_t *current = node;
+
+	if (head == NULL)
+		return (1);
+	if (current == NULL)
+		return (1);
+	check1 = is_helper(head, current->next);
+	if (check1 == 0)
+		return (0);
+	check2 = (current->n = (*head)->n);
+	*head = (*head)->next;
+	return (check2);
+}
+
+/**
+ * is_palindrome - function that checks if singly linked list is a palindrome
+ * @head: head of singly linked list
+ * Description: 13. Linked list palindrome
+ * Return: see below
+ * 1. upon success, 1 if it's palindrome or empty list
+ * 2. upon fail, 0 if it isn't a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	int check = 0;
+		check = is_helper(head, *head);
+	return (check);
+}
