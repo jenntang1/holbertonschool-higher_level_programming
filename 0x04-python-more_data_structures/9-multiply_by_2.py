7965189dc6c46cd666c9e4f9650deac7cb9511f9
jenntang1 <1039@holbertonschool.com>
2019-12-19 21:50:55 +0000

    2. Updating to be in compliance with pep8, jenntang1

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = a_dictionary.copy()
-    new_dictionary.update((key, value*2) for key, value in new_dictionary.items())
-    return new_dictionary
+    new_dict = a_dictionary.copy()
+    new_dict.update((key, value*2) for key, value in new_dict.items())
+    return new_dict
