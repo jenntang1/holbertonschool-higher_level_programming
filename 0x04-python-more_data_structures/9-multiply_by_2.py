a17f904d09e2d7a2f34ea151063001713b9686e6
jenntang1 <1039@holbertonschool.com>
2019-12-19 21:48:55 +0000

    1. Creating function that multiplies every value in dictionary by 2, jenntang1

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = a_dictionary.copy()
+    new_dictionary.update((key, value*2) for key, value in new_dictionary.items())
+    return new_dictionary
