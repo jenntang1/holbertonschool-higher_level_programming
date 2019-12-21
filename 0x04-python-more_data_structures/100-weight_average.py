1d1c389cce73511f065f1e269e3d75fe26be8307
jenntang1 <1039@holbertonschool.com>
2019-12-21 05:14:59 +0000

    Creating function that returns the weighted average, jenntang1

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    weights = 0
+    weighted_scores = []
+    for element in my_list:
+        weights += element[1]
+        weighted_scores.append(element[0] * element[1])
+    return sum(weighted_scores) / weights
