bf0f83e82be8cba13998f0c1f8475d32c7e34bbb
jenntang1 <1039@holbertonschool.com>
2019-12-21 05:18:09 +0000

    2. Updating to account for an empty list, jenntang1

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,10 @@
 def weight_average(my_list=[]):
     weights = 0
     weighted_scores = []
-    for element in my_list:
-        weights += element[1]
-        weighted_scores.append(element[0] * element[1])
-    return sum(weighted_scores) / weights
+    if my_list:
+        for element in my_list:
+            weights += element[1]
+            weighted_scores.append(element[0] * element[1])
+        return sum(weighted_scores) / weights
+    else:
+        return 0
