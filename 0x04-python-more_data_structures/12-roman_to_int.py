0ec13eeef7b38a8e2df38493a60667082820a191
jenntang1 <1039@holbertonschool.com>
2019-12-23 04:47:58 +0000

    3. Updating to account for no string is given or not a string, jenntang1

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,14 @@ def roman_to_int(roman_string):
     converted = []
     compare = []
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    for index1 in range(len(roman_string)):
-        for key, value in dict.items():
-            if roman_string[index1] in key:
-                converted.append(value)
-    compare = converted[1:]
-    for index2 in range(len(roman_string) - 1):
-        if converted[index2] < compare[index2]:
-            converted[index2] = converted[index2] * -1
-    return sum(converted)
+    if roman_string:
+        for index1 in range(len(roman_string)):
+            for key, value in dict.items():
+                if roman_string[index1] in key:
+                    converted.append(value)
+        compare = converted[1:]
+        for index2 in range(len(roman_string) - 1):
+            if converted[index2] < compare[index2]:
+                converted[index2] = converted[index2] * -1
+        return sum(converted)
+    return 0
