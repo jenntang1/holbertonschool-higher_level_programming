c1957be32a0fd9129d057c641d47037ef181f1a8
jenntang1 <1039@holbertonschool.com>
2020-01-10 15:53:21 +0000

    2. Updating to correct return result statement, jenntang1

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ def safe_function(fct, *args):
     from sys import stderr
     try:
         result = fct(*args)
-        return fct
+        return result
     except Exception as err:
         print("Exception: {}".format(err), file=stderr)
         return None
