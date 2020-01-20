1b7e7f30bf3b176ac0b7d9cfd37d3f706635340a
jenntang1 <1039@holbertonschool.com>
2020-01-20 18:39:47 +0000

    1. Adding a public instance method to the BaseGeometry class, jenntang1

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 6. Improve Geometry """
+
+
+class BaseGeometry:
+    """ Creating a class called BaseGeometry. """
+    pass
+
+    def area(self):
+        """ Creating a public instance method.
+        Raises:
+            Exception: "area() is not implemented"
+        """
+        raise Exception("area() is not implemented")
