db26827ef23105c1dfb60463c29e764848ff9350
jenntang1 <1039@holbertonschool.com>
2020-01-21 23:24:12 +0000

    1. Creating function that creates and returns an object from a JSON file, jenntang1

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 8. Create object from a JSON file """
+
+
+def load_from_json_file(filename):
+    """ Creating a function that creates an object from a
+    JSON file.
+    Note:
+        The with statement opens, reads and closes a file
+    Args:
+        filename: name of JSON file
+    """
+    import json
+    with open(filename, 'r+') as file_obj:
+        decoded = json.load(file_obj)
+        return(decoded)
