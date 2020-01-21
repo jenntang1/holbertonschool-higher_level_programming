28c473b9a529b59752b752bbbc06278b02815512
jenntang1 <1039@holbertonschool.com>
2020-01-21 01:34:20 +0000

    1. Creating a class called Square that prints its description, jenntang1

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+""" 11. Square #2"""
+
+
+class BaseGeometry:
+    """ Creating a class called BaseGeometry. """
+
+    def area(self):
+        """ Creating a public instance method.
+        Raises:
+            Exception: if there's no area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Creating a public instance method.
+        Note:
+            Validates a value
+        Args:
+            name: always a string
+            value: an integer type
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than or equal to 0
+        """
+        if isinstance(name, str) is False:
+            raise TypeError("{} must be an integer".format(name))
+        if isinstance(value, int) is False:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Creating a subclass called Rectangle that inherits
+    from class BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+        """ Using the __init__ method.
+        Note:
+            Args are validated by the integer_validator method
+        Args:
+            width: private attribute that's a positive integer
+            height: private attribute that's a positive integer
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+        if type(self.__width) is not int:
+            raise TypeError("{} must be an integer".format(self.__width))
+        if type(self.__height) is not int:
+            raise TypeError("{} must be an integer".format(self.__height))
+        if self.__width <= 0:
+            raise ValueError("{} must be greater than 0".format(self.__width))
+        if self.__height <= 0:
+            raise ValueError("{} must be greater than 0".format(self.__height))
+
+    def area(self):
+        """ Implementing the area method.
+        Returns:
+            The area of the Rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ Using the __str__ method.
+        Returns:
+            Description of the Rectangle
+        """
+        class_name = type(self).__name__
+        return("[{}] {}/{}".format(class_name, self.__width, self.__height))
+
+
+class Square(Rectangle):
+    """ Creating a multi-level subclass called Square that inherits
+    from subclass Rectangle.
+    """
+
+    def __init__(self, size):
+        """ Using the __init__ method.
+        Note:
+            Size is validated by the integer_validator method
+        Args:
+            size: private attribute that's a positive integer
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ Implementing the area method.
+        Returns:
+            The area of the Square
+        """
+        return self.__size * self.__size
+
+    def __str__(self):
+        """ Using the __str__ method.
+        Returns:
+            Description of the Square
+        """
+        parent_name = type(self).__name__
+        return("[{}] {}/{}".format(parent_name, self.__size, self.__size))
