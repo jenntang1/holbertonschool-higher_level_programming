f7f55f9918b5754b65d3ad0711b0b15429f73361
jenntang1 <1039@holbertonschool.com>
2020-01-22 00:57:22 +0000

    1. Creating a class called Student that defines a student with public instance attributes, jenntang1

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 11. Student to JSON """
+
+
+class Student:
+    """ Creating a class called Student that defines a student. """
+
+    def __init__(self, first_name, last_name, age):
+        """ Using __init__ method.
+        Args:
+            first_name: public instance attribute
+            last_name: public instance attribute
+            age: publict instance attribute
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Creating a public instance method.
+        Note:
+            Retrieves a dict representation of a Student instance
+        """
+        return (self.__dict__)
