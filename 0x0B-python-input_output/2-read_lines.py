cde61f4bcc82cbe9f94cb7382e3e0d04987b69aa
jenntang1 <1039@holbertonschool.com>
2020-01-22 03:19:12 +0000

    4. Updating to correctly account for requirements, jenntang1

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -6,16 +6,20 @@ def read_lines(filename="", nb_lines=0):
     """ Creating a function that reads n lines of
     a text file and prints to stdout.
     Note:
-        The with statement opens, reads and closes a file
+        The with statement opens, reads and closes a file.
+        Reads entire file:
+            if nb_lines is lower or equal to 0
+            if nb_lines is greater or equal to total lines
     Args:
         filename: name of text file
         nb_lines: number of lines to read
     """
-    text_count = 0
     text_content = ""
     with open(filename, 'r') as file_obj:
-        for text_line in file_obj:
-            text_count += 1
-        if nb_lines <= 0 or nb_lines >= text_count:
+        if nb_lines <= 0:
             text_content = file_obj.read()
-            print("{}".format(text_content))
+            print("{}".format(text_content), end="")
+        else:
+            for text_line in range(nb_lines):
+                text_content = file_obj.readline()
+                print("{}".format(text_content), end="")
