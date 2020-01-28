3349f5a1cc5a64217d780d1a967d65dbcebd93ab
jenntang1 <1039@holbertonschool.com>
2020-01-28 18:08:36 +0000

    6. Updating to correct getter and setter for size, jenntang1

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,7 +24,6 @@ class Square(Rectangle):
             y: inherited attribute
             id: inherited attribute
         """
-        self.size = size
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
@@ -45,7 +44,7 @@ class Square(Rectangle):
         Returns:
             size
         """
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -60,7 +59,8 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__size = value
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """ Creating public instance method.
