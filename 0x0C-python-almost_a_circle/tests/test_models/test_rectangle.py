fab3e64581fa5716d21e9fc57c2b28b1adece7cd
jenntang1 <1039@holbertonschool.com>
2020-01-29 03:16:17 +0000

    2. Updating to include more test cases, jenntang1

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,7 +2,6 @@
 """ 0. If it's not tested it doesn't work """
 from models.rectangle import Rectangle
 import unittest
-import random
 import os
 
 
@@ -10,38 +9,48 @@ class MyTest(unittest.TestCase):
     """ Creating class to test Rectangle class. """
 
     def setUp(self):
-        self.id = 1
+        """ Defaults """
         self.width = 1
         self.height = 1
         self.x = 1
         self.y = 1
+        self.id = 1
 
-    def test_int_id(self):
-        """ Check id is int """
+    def test_all(self):
+        """ Checking all attributes """
         test = Rectangle(1, 2, 3, 4, 5)
         self.assertEqual(test.id, 5)
 
-    def test_int_width(self):
-        """ Check width is int """
-        test = Rectangle(1, 2, 3, 4, 5)
+    def test_two(self):
+        """ Checking two attributess """
+        test = Rectangle(1, 2)
         self.assertEqual(test.width, 1)
 
-    def test_int_height(self):
-        """ Check height is int """
-        test = Rectangle(1, 2, 3, 4, 5)
+    def test_three(self):
+        """ Checking three attributes """
+        test = Rectangle(1, 2, 3)
         self.assertEqual(test.height, 2)
 
-    def test_int_x(self):
-        """ Check x is int """
-        test = Rectangle(1, 2, 3, 4, 5)
+    def test_four(self):
+        """ Checking four attributes """
+        test = Rectangle(1, 2, 3, 4)
         self.assertEqual(test.x, 3)
 
-    def test_int_y(self):
-        """ Check y is int """
-        test = Rectangle(1, 2, 3, 4, 5)
-        self.assertEqual(test.y, 4)
+    def test_raise_one(self):
+        """ Checking width TypeError """
+        self.assertRaises(TypeError, Rectangle, ("1", 2))
 
+    def test_raise_two(self):
+        """ Checking height TypeError """
+        self.assertRaises(TypeError, Rectangle, (1, "2"))
 
+    def test_raise_three(self):
+        """ Checking x TypeError """
+        self.assertRaises(TypeError, Rectangle, (1, 2, "3"))
+
+    def test_raise_four(self):
+        """ Checking y TypeError """
+        self.assertRaises(TypeError, Rectangle, (1, 2, 3, "4"))
 
 if __name__ == "__main__":
     unittest.main()
