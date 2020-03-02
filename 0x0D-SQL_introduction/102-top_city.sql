6cb4cac44463d9d2455e5028a694472a1cdea70f
jenntang1 <1039@holbertonschool.com>
2020-03-02 19:02:59 +0000

    2. Updating to include WHERE clause to find the 3 cities with the highest temperature during July and August

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Displays top 3 cities with highest average temperature.
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month between '07' and '08' GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
