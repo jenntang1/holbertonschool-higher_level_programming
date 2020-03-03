c0d7f2efd6663912df1d4c4a518df51076c75fbe
jenntang1 <1039@holbertonschool.com>
2020-03-03 21:50:34 +0000

    5. Updating to change for to FOR and add string literals

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges for specific users.
-SHOW GRANTS for user_0d_1@localhost;
-SHOW GRANTS for user_0d_2@localhost;
+SHOW GRANTS for 'user_0d_1'@'localhost';
+SHOW GRANTS for 'user_0d_2'@'localhost';
