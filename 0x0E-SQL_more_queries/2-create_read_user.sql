58b4b86b1101c53cb855726c8b59ac6090da57d9
jenntang1 <1039@holbertonschool.com>
2020-03-03 19:26:16 +0000

    3. Updating to remove string literals

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- Creates a database and grants user access.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-DROP USER 'user_0d_2'@'localhost';
+DROP USER user_0d_2@localhost;
 FLUSH PRIVILEGES;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
