e9d9bd6fd999f3af2c5d33b033e3ec071dd659c4
jenntang1 <1039@holbertonschool.com>
2020-03-03 20:29:19 +0000

    2. Updating to correct SELECT command

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- Lists a field from a database table.
-SELECT states.id, cities.name
+SELECT cities.id, cities.name
 FROM states, cities
 WHERE states.name = 'California'
 AND states.id = cities.state_id
