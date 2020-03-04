725ea71ec70f669c0830a666d481a11f9e416a32
jenntang1 <1039@holbertonschool.com>
2020-03-04 02:10:18 +0000

    2. Updating aliases to include AS instead of a space

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Lists all tv shows and all the genres they belong to.
 SELECT c.title, a.name
-FROM tv_genres a
-RIGHT JOIN tv_show_genres b ON a.id = b.genre_id
-RIGHT JOIN tv_shows c ON b.show_id = c.id
+FROM tv_genres AS a
+RIGHT JOIN tv_show_genres AS b ON a.id = b.genre_id
+RIGHT JOIN tv_shows AS c ON b.show_id = c.id
 ORDER BY c.title, a.name ASC
