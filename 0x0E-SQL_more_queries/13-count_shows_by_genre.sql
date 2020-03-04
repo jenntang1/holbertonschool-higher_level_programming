b56aa0460978380bd0b52e20c7ad7521a7c05c99
jenntang1 <1039@holbertonschool.com>
2020-03-04 00:14:42 +0000

    3. Updating to change GROUP BY clause from using a.name to genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT a.name AS genre, COUNT(b.show_id) AS number_of_shows
 FROM tv_genres a
 LEFT JOIN tv_show_genres b
 ON a.id = b.genre_id
-GROUP BY a.name
+GROUP BY genre
 ORDER BY number_of_shows DESC;
