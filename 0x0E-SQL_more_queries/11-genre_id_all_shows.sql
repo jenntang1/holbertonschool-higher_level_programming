7d8d891a469a7b7a7477b75236a3c85c94a21e07
jenntang1 <1039@holbertonschool.com>
2020-03-04 01:53:31 +0000

    2. Updating to use RIGHT JOIN

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,8 +1,7 @@
--- Lists data given one requirement.
+-- Lists all tv shows and all their genre id's.
 SELECT c.title, b.genre_id
 FROM tv_show_genres b
-LEFT JOIN tv_shows c
+RIGHT JOIN tv_shows c
 ON b.show_id = c.id
-OR b.show_id IS NULL
 GROUP BY c.title, b.genre_id
 ORDER BY c.title, b.genre_id ASC;
