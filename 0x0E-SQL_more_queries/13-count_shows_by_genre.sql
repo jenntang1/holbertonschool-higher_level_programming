8e4148f2ecb99a7e28b9043be20c2c6300eb833d
jenntang1 <1039@holbertonschool.com>
2020-03-04 00:11:14 +0000

    2. Updating to correct first label from name to genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists the genre and the number of tv shows that belongs to the genre.
-SELECT a.name, COUNT(b.show_id) AS number_of_shows
+SELECT a.name AS genre, COUNT(b.show_id) AS number_of_shows
 FROM tv_genres a
 LEFT JOIN tv_show_genres b
 ON a.id = b.genre_id
