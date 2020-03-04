7f3d1fd5aae1967a633e94b54b840e4b0a8e6b02
jenntang1 <1039@holbertonschool.com>
2020-03-04 02:09:37 +0000

    5. Updating aliases to include AS instead of a space

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Lists the genre and the number of tv shows that belongs to the genre.
 SELECT a.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres a
-LEFT JOIN tv_show_genres b
+FROM tv_genres AS a
+RIGHT JOIN tv_show_genres AS b
 ON a.id = b.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
