a066fe769182b80d6fda8dd31d748662e21e2cb8
jenntang1 <1039@holbertonschool.com>
2020-03-04 02:14:46 +0000

    6. Updating to use field name in COUNT

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists the genre and the number of tv shows that belongs to the genre.
-SELECT a.name AS genre, COUNT(*) AS number_of_shows
+SELECT a.name AS genre, COUNT(b.show_id) AS number_of_shows
 FROM tv_genres AS a
 RIGHT JOIN tv_show_genres AS b
 ON a.id = b.genre_id
