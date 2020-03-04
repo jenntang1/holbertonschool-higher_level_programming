e085a16a1698af10994732bc216428e8ba2101a3
jenntang1 <1039@holbertonschool.com>
2020-03-04 02:10:04 +0000

    2. Updating aliases to include AS instead of a space

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
 -- Lists all tv shows that belongs to the Comedy genre.
 SELECT c.title
-FROM tv_genres a, tv_show_genres b, tv_shows c
+FROM tv_genres AS a, tv_show_genres AS b, tv_shows AS c
 WHERE a.name = "Comedy"
 AND b.show_id = c.id
 AND b.genre_id = a.id
