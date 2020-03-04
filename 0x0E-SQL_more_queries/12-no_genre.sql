7e5131f13e05538b882e60d64f0226998a5d6967
jenntang1 <1039@holbertonschool.com>
2020-03-04 02:09:14 +0000

    3. Updating aliases to include AS instead of a space

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all tv shows without a genre.
 SELECT c.title, b.genre_id
-FROM tv_show_genres b
-RIGHT JOIN tv_shows c
+FROM tv_show_genres AS b
+RIGHT JOIN tv_shows AS c
 ON b.show_id = c.id
 WHERE b.genre_id IS NULL
 ORDER BY c.title, b.genre_id ASC;
