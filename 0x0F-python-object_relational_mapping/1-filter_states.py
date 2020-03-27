a105664779aa0f028b5a0656c9bd1189c828e18c
jenntang1 <1039@holbertonschool.com>
2020-03-27 20:29:44 +0000

    2. Updating to select all records

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     cursor = connection.cursor()
 
-    cursor.execute("SELECT name FROM states WHERE name LIKE 'N%'")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
 
     all_states = cursor.fetchall()
 
