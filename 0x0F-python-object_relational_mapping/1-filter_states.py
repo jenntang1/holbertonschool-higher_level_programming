47fca0ab0dacc85f0cbe8f9812c15bf4ea616ce8
jenntang1 <1039@holbertonschool.com>
2020-03-27 20:20:12 +0000

    1. Creating python script that lists states starting with 'N'

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" 1. Filter states """
+
+
+if __name__ == "__main__":
+
+    from sys import argv
+    import MySQLdb
+
+    connection = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        host="localhost",
+        port=3306
+    )
+
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT name FROM states WHERE name LIKE 'N%'")
+
+    all_states = cursor.fetchall()
+
+    for one_state in all_states:
+        print(one_state)
+
+    cursor.close()
+
+    connection.close()
