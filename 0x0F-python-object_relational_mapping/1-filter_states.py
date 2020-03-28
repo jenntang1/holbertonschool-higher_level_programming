ba8da2675b039dcda1c0a3f46f12e432df25deea
jenntang1 <1039@holbertonschool.com>
2020-03-28 16:44:44 +0000

    5. Changing execute to explicit records

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,10 +18,8 @@ if __name__ == "__main__":
     cursor = connection.cursor()
 
     cursor.execute("SELECT states.id, states.name "
-                   "FROM INFORMATION_SCHEMA.TABLES, states "
-                   "WHERE TABLE_SCHEMA = 'hbtn_0e_0_usa' "
-                   "AND TABLE_NAME = 'states' "
-                   "AND states.name LIKE 'N%'"
+                   "FROM states "
+                   "WHERE states.name LIKE 'N%' "
                    "ORDER BY states.id")
 
     data = cursor.fetchall()
