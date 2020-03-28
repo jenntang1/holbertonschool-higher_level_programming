e013346f2b76e447507b884113470c972d7df9c0
jenntang1 <1039@holbertonschool.com>
2020-03-28 21:14:21 +0000

    4. Reordering import modules

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,17 +6,15 @@ if __name__ == "__main__":
 
     from sys import argv
     from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-    from sqlalchemy.orm import sessionmaker
-
     class_session = sessionmaker(bind=engine)
 
     session = class_session()
-
-    from model_state import Base, State
 
     for result in session.query(State).order_by(State.id).all():
         print("{}: {}".format(result.id, result.name))
