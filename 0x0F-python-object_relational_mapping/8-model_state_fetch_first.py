c8c56249186a2e435ce84c5f396a24c34949b2ea
jenntang1 <1039@holbertonschool.com>
2020-03-28 17:23:41 +0000

    1. Creating sqlalchemy model to get first record

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 8. First state """
+
+
+if __name__ == "__main__":
+
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.orm.exc import NoResultFound
+    from model_state import Base, State
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    class_session = sessionmaker(bind=engine)
+
+    session = class_session()
+
+    result = session.query(State).first()
+
+    if result is not None:
+        print("{}: {}".format(result.id, result.name))
+    else:
+        print("Nothing")
+
+    session.close()
