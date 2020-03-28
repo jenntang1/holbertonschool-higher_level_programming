7cd9292b761953b0c4419b719ec0c9d9d1a9aec1
jenntang1 <1039@holbertonschool.com>
2020-03-28 01:26:23 +0000

    1. Creating python script that lists all cities in a state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" 5. All cities by state """
+
+
+if __name__ == "__main__":
+
+    from sys import argv
+    import MySQLdb
+
+    connection = MySQLdb.connect(
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        host="localhost",
+        port=3306
+    )
+
+    cursor = connection.cursor()
+
+    query = ("SELECT cities.name "
+             "FROM states, cities "
+             "WHERE cities.name IS NOT NULL "
+             "AND states.id = cities.state_id "
+             "AND states.name = '%s' "
+             "ORDER BY cities.id" % argv[4])
+
+    cursor.execute(query)
+
+    data = cursor.fetchall()
+
+    for result in data:
+        print(result[0])
+
+    cursor.close()
+
+    connection.close()
