134f721f7c06c189cc85153a1beff2ff9a719211
jenntang1 <1039@holbertonschool.com>
2020-04-04 15:42:28 +0000

    Revert "1. Creating directory to help with debugging a web stack"
    This reverts commit 0814b88d0c2bccf561da8077b3a4046e870e343b.

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     from sys import argv
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
-    from relationship_state import Base, State
-    from relationship_city import City
+    from model_state import Base, State
+    from model_city import City
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
