cf7bbc75ffd6c5f4717df089e763d98e010cbdc2
jenntang1 <1039@holbertonschool.com>
2020-04-07 16:18:07 +0000

    2. Updating with silent flag

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This bash script takes in a URL, sends GET request and displays the body.
-curl "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
+curl -sX GET "$1" -H "X-HolbertonSchool-User-Id: 98"
