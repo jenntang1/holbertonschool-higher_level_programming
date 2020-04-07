46322b2f8eb0004c87df4cb859f3e47aa841e229
jenntang1 <1039@holbertonschool.com>
2020-04-07 17:58:36 +0000

    2. Updating using grep

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This bash script takes in a URL, sends a request to that URL and displays the body of 200.
-if curl -sI "$1" | grep -q '200 OK'
-then
-	curl -s "$1"
-fi
+curl -sIX GET "$1" | grep 200
