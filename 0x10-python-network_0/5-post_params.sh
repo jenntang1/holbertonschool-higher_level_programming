76b589124935dc57a02380a7267ddf506fe931c7
jenntang1 <1039@holbertonschool.com>
2020-04-07 16:21:37 +0000

    3. Changing colon to equal to set key value

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This bash script takes in a URL, sends POST request and displays the body.
-curl -sX POST "$1" -d "email: hr@holbertonschool.com&subject: I will always be here for PLD"
+curl -sX POST "$1" -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
