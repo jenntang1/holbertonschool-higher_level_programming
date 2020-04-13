3086d3465c05456dfa0138d8e1991348a3c0fb3a
jenntang1 <1039@holbertonschool.com>
2020-04-13 20:40:22 +0000

    3. Changing to simplifying the dictionary to POST

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,11 +8,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-
-    values = {}
-    values['email'] = email
-
+    values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values).encode('utf-8')
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
