302d09493f691002267c4b9943c93fb530e11425
jenntang1 <1039@holbertonschool.com>
2020-04-13 21:02:02 +0000

    5. Removing extra text in print statement

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     values = {'email': sys.argv[2]}
-    request = urllib.parse.urlencode(values).encode('ascii')
+    request = urllib.parse.urlencode(values).encode('utf-8')
     with urllib.request.urlopen(url, data=request) as response:
         body = response.read().decode('utf-8')
-        print("Your email is: {}".format(body))
+        print("{}".format(body))
