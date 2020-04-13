0ab1e4e70d953bef29ac56feca2e16f84dfbc02f
jenntang1 <1039@holbertonschool.com>
2020-04-13 22:20:56 +0000

    3. Removing url variable to simplify code

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,6 +7,5 @@ import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    response = requests.get(url)
-    print("{}".format(response.headers['X-Request-Id']))
+    response = requests.get(sys.argv[1])
+    print(response.headers['X-Request-Id'])
