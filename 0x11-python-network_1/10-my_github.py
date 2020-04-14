3a4635c943735d23564f1aa5dd9d017d0ffc1fdc
jenntang1 <1039@holbertonschool.com>
2020-04-14 14:33:59 +0000

    2. Correcting github url

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,13 +4,18 @@
 
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    github = 'https://api.github.com/'
+    github = 'https://api.github.com/users/'
     url = github + username
-    response = requests.get(url, auth=(username, password))
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
     json_dict = response.json()
-    print("{}".format(json_dict["id"]))
+    for key, value in json_dict.items():
+        if key == 'id':
+            print("{}".format(value))
+    else:
+        print("None")
