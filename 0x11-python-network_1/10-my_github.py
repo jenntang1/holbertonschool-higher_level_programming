28d5ca1067f6fb4e851778247719f23bd5859656
jenntang1 <1039@holbertonschool.com>
2020-04-17 17:33:14 +0000

    3. Adding authorization for header request

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,12 +10,10 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    github = 'https://api.github.com/users/'
-    url = github + username
-    response = requests.get(url, auth=HTTPBasicAuth(username, password))
-    json_dict = response.json()
-    for key, value in json_dict.items():
-        if key == 'id':
-            print("{}".format(value))
+    url = 'https://api.github.com/user'
+    auth = {'Authorization': 'token {}'.format(password)}
+    response = requests.get(url, headers=auth).json()
+    if response.get('id'):
+        print("{}".format(response.get('id')))
     else:
         print("None")
