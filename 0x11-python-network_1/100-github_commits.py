5133b9a1393c977a5353e182afa88d059d353582
jenntang1 <1039@holbertonschool.com>
2020-04-17 21:05:09 +0000

    1. Creating Python script to get last 10 commits from a Github repo

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Search for Last 10 Commits in a Github Repo """
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    response = requests.get(url).json()
+    for item in range(0, 10):
+        print("{}: {}".format(response[item].get('sha'),
+                              response[item].get('commit')
+                              .get('author').get('name')))
