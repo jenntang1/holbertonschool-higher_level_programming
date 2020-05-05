bc70587fec69e340fe76019290b5ab897bcf47ac
jenntang1 <1039@holbertonschool.com>
2020-05-05 20:37:49 +0000

    1. Creating js script that finds the second largest integer in list/array

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// Searches for the second largest integer in list
+// If no argument passed, print "0"
+// If only one argument passed, print "0"
+
+const myArgs = process.argv.slice(2);
+const length = myArgs.length;
+
+if (length <= 1) {
+  console.log('0');
+} else {
+  let i, j, swap;
+  for (i = 0; i < length; i++) {
+    for (j = i + 1; j < length; j++) {
+      if (myArgs[i] > myArgs[j]) {
+        swap = myArgs[i];
+        myArgs[i] = myArgs[j];
+        myArgs[j] = swap;
+      }
+    }
+  }
+  console.log(myArgs[length - 2]);
+}
