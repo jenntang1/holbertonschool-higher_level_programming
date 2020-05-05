883680ff99e855f9a2e7cab50a6fa5cb5af9c510
jenntang1 <1039@holbertonschool.com>
2020-05-05 19:54:54 +0000

    1. Creating js function that computes a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// Computes and prints a factorial
+// Formula: n! = n × (n−1)!
+// Example: 4! = 4 x 3 x 2 x 1 = 24
+// Note: 1. ignore negative numbers
+//       2. 0! = 1
+//       3. 1! = 1
+
+const myArgs = process.argv.slice(2);
+
+function factorial (num) {
+  if (parseInt(num) === 0 || parseInt(num) === 1 || isNaN(num)) {
+    return 1;
+  } else if (parseInt(num) < 0) {
+    //pass
+  } else {
+    return parseInt(num) * factorial(parseInt(num) - 1);
+  }
+}
+
+console.log(factorial(myArgs[0]));
