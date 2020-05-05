b5a724c40c67fb5e622886e15c6f5e896bf02394
jenntang1 <1039@holbertonschool.com>
2020-05-05 21:21:08 +0000

    3. Creating new array to hold converted array

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,25 @@
 #!/usr/bin/node
 // Finds the second largest integer in list
-// Sorts the list and returns the second to last element
+// Bubble sorts the list and returns the second to last element
 // If no argument passed, print "0"
 // If only one argument passed, print "0"
 
 const myArgs = process.argv.slice(2);
 const length = myArgs.length;
+const newArr = myArgs.map(Number);
 
 if (length <= 1) {
   console.log('0');
 } else {
   let i, j, swap;
-  for (i = 0; i < length; i++) {
-    for (j = i + 1; j < length; j++) {
-      if (myArgs[i] > myArgs[j]) {
-        swap = myArgs[i];
-        myArgs[i] = myArgs[j];
-        myArgs[j] = swap;
+  for (i = 0; i < length - 1; i++) {
+    for (j = 0; j < length - i - 1; j++) {
+      if (newArr[j] > newArr[j + 1]) {
+        swap = newArr[j];
+        newArr[j] = newArr[j + 1];
+        newArr[j + 1] = swap;
       }
     }
   }
-  console.log(myArgs[length - 2]);
+  console.log(newArr[length - 2]);
 }
