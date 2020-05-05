25f661a5a9246f137468f08db87972c83d521690
jenntang1 <1039@holbertonschool.com>
2020-05-05 18:03:58 +0000

    4. Updating to print only the first argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // If no arguments are passed, prints “No argument”
-// If an argument is passed, prints all  arguments
+// If an argument is passed, prints the first argument
 
 const myArgs = process.argv.slice(2);
 
 if (myArgs[0] === undefined) {
   console.log('No argument');
 } else {
-  myArgs.forEach(element => console.log(element));
+  console.log(myArgs[0]);
 }
