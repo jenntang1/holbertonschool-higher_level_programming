328e8ccd7f851c4668054c4c6be0f51eab98c183
jenntang1 <1039@holbertonschool.com>
2020-05-05 17:25:43 +0000

    Update with a loop to print all arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
 // If no arguments are passed, prints “No argument”
-// If an argument is passed, prints the first argument
+// If an argument is passed, prints all  arguments
 
 const myArgs = process.argv.slice(2);
 const length = myArgs.length;
+let i = 0;
 
 if (length === 0) {
   console.log('No argument');
 } else {
-  console.log(myArgs[0]);
+  for (; i < length; i++) {
+    console.log(myArgs[i]);
+  }
 }
