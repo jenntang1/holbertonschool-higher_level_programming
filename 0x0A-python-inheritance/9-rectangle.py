cb69de93c8dae29ed208944d304de080273f81c8
jenntang1 <1039@holbertonschool.com>
2020-05-07 01:59:39 +0000

    2. Removing code from prior task and importing them instead

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -2,33 +2,7 @@
 """ 9. Full rectangle """
 
 
-class BaseGeometry:
-    """ Creating a class called BaseGeometry. """
-
-    def area(self):
-        """ Creating a public instance method.
-        Raises:
-            Exception: if there's no area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Creating a public instance method.
-        Note:
-            Validates a value
-        Args:
-            name: always a string
-            value: an integer type
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than or equal to 0
-        """
-        if isinstance(name, str) is False:
-            raise TypeError("{} must be an integer".format(name))
-        if isinstance(value, int) is False:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -48,15 +22,6 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-        if type(self.__width) is not int:
-            raise TypeError("{} must be an integer".format(self.__width))
-        if type(self.__height) is not int:
-            raise TypeError("{} must be an integer".format(self.__height))
-        if self.__width <= 0:
-            raise ValueError("{} must be greater than 0".format(self.__width))
-        if self.__height <= 0:
-            raise ValueError("{} must be greater than 0".format(self.__height))
 
     def area(self):
         """ Implementing the area method.
