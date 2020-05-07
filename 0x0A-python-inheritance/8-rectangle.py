782c79ecfaa13ddecbe5b66f63033bbd5c4b2bf0
jenntang1 <1039@holbertonschool.com>
2020-05-07 01:53:05 +0000

    3. Removing code from prior task and importing them instead

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,33 +2,7 @@
 """ 8. Rectangle """
 
 
-class BaseGeometry:
-    """ Creating a class called BaseGeometry. """
-
-    def area(self):
-        """ Creating a public instance method.
-        Raises:
-            Exception: if there's no area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Creating a public instance method.
-        Note:
-            Validates a value
-        Args:
-            name: always a string
-            value: an integer type
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than or equal to 0
-        """
-        if isinstance(name, str) is False:
-            raise TypeError("{} must be an integer".format(name))
-        if isinstance(value, int) is False:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
