b227cc06c3ff60e308bf97266055944715da0e97
jenntang1 <1039@holbertonschool.com>
2020-05-11 19:31:29 +0000

    4. Removing this initialization

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,12 +5,7 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    if (size <= 0) {
-      return new Square();
-    } else {
-      super(size, size);
-      this.size = size;
-    }
+    super(size, size);
   }
 }
 
