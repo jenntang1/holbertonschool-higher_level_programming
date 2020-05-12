a02f9e5600d7aeb2f215adc696cd5f90cc208881
jenntang1 <1039@holbertonschool.com>
2020-05-12 20:37:48 +0000

    5. Updating to use fs.open

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,12 +8,21 @@ const myArgs = process.argv.slice(2);
 // If there's an error, then print the error object
 // If not, print the content of the file
 const fs = require('fs');
-fs.readFile(myArgs[0], 'utf-8', function (error, content) {
-  if (error) {
+
+fs.open(myArgs[0], 'r', function (error, stat) {
+  if (error === null) {
+    fs.readFile(myArgs[0], 'utf-8', function (error, content) {
+      if (error) {
+        const errMsg = { Error: error.message };
+        const obj = Object.assign({}, errMsg, error);
+        console.log(obj);
+      } else if (content) {
+        process.stdout.write(content);
+      }
+    });
+  } else if (error.code === 'ENOENT') {
     const errMsg = { Error: error.message };
     const obj = Object.assign({}, errMsg, error);
     console.log(obj);
-  } else if (content) {
-    process.stdout.write(content);
   }
 });
