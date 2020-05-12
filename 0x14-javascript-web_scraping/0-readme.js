87d1fe78423b88222ec470e74cc0fb7d15213d38
jenntang1 <1039@holbertonschool.com>
2020-05-12 23:36:53 +0000

    6. Updating to use fs.close

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -8,21 +8,23 @@ const myArgs = process.argv.slice(2);
 // If there's an error, then print the error object
 // If not, print the content of the file
 const fs = require('fs');
+const file = myArgs[0];
 
-fs.open(myArgs[0], 'r', function (error, stat) {
-  if (error === null) {
-    fs.readFile(myArgs[0], 'utf-8', function (error, content) {
+fs.open(file, 'r', (error, fd) => {
+  if (error) {
+    return console.error(error);
+  } else {
+    fs.readFile(file, 'utf-8', (error, content) => {
       if (error) {
-        const errMsg = { Error: error.message };
-        const obj = Object.assign({}, errMsg, error);
-        console.log(obj);
-      } else if (content) {
+        return console.error(error);
+      } else {
         process.stdout.write(content);
       }
     });
-  } else if (error.code === 'ENOENT') {
-    const errMsg = { Error: error.message };
-    const obj = Object.assign({}, errMsg, error);
-    console.log(obj);
   }
+  fs.close(fd, (error) => {
+    if (error) {
+      return console.error(error);
+    }
+  });
 });
