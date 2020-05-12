a07a38ff64e68c7df557a99c14bf537c0187e7f9
jenntang1 <1039@holbertonschool.com>
2020-05-12 15:35:53 +0000

    1. Creating script to find the number of movies Wedge Antilles appeared in

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Prints number of movies Wedge Antilles appeared in
+
+// Reads and saves command line args to a list
+const myArgs = process.argv.slice(2);
+
+// Imports request module to make http call
+// https://swapi-api.hbtn.io/api/films/ will be the argument passed
+const request = require('request');
+const url = myArgs[0].replace('films', 'people/18');
+request.get(url, { json: true }, function (error, response, body) {
+  if (body) {
+    console.log(body.films.length);
+  } else {
+    console.error(error);
+  }
+});
