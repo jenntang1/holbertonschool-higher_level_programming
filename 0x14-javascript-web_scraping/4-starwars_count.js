293b5894e39c079f3ad3512f9f8fb59f45691f54
jenntang1 <1039@holbertonschool.com>
2020-05-13 23:00:05 +0000

    4. Updating endpoint

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,13 +7,24 @@ const myArgs = process.argv.slice(2);
 // Imports request module to make http call
 // https://swapi-api.hbtn.io/api/films/ will be the argument passed
 const request = require('request');
-const url = myArgs[0].replace('films', 'people/18');
+const url = myArgs[0];
 request.get(url, { json: true }, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else if (body.films) {
-    console.log(body.films.length);
   } else {
-    console.log(error);
+    let count = 0;
+    let elmt = 0;
+    let links;
+    const list = body.results;
+    for (; elmt < list.length; elmt++) {
+      if (list[elmt].characters) {
+        for (links of list[elmt].characters) {
+          if (links.includes('18')) {
+            count++;
+          }
+        }
+      }
+    }
+    console.log(count);
   }
 });
