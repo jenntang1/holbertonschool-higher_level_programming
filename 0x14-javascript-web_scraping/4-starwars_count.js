a2a178bf30d09c70c33afc9b5a581f7847bfed8b
jenntang1 <1039@holbertonschool.com>
2020-05-13 02:13:58 +0000

    3. Updating with another error handling

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,10 +8,12 @@ const myArgs = process.argv.slice(2);
 // https://swapi-api.hbtn.io/api/films/ will be the argument passed
 const request = require('request');
 const url = myArgs[0].replace('films', 'people/18');
-request.get(url, { json: true }, function (error, response, body) {
+request.get(url, { json: true }, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else if (body.films) {
     console.log(body.films.length);
+  } else {
+    console.log(error);
   }
 });
