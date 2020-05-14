6f2f564c53f985cb10c3b394afd81a19247eb581
jenntang1 <1039@holbertonschool.com>
2020-05-14 05:16:03 +0000

    2. Simplifying code

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,7 +2,7 @@
 // Function converts a number from base 10 to another base passed as argument
 
 exports.converter = function (base) {
-  return function (base) {
-    return base.toString(8);
+  return function (val) {
+    return val.toString(base);
   };
 };
