e0bbd66a1ccde948fe102cbbaa790f0a61319ab2
jenntang1 <1039@holbertonschool.com>
2020-05-15 19:29:12 +0000

    2. Updating with ready

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Fetches and return 'Salut' (French for 'hi')
 
-$(function () {
+$(document).ready(function () {
   $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
     $('DIV#hello').text(data.hello);
   });
