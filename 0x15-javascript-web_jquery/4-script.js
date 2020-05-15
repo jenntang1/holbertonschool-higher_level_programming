958d5dc603d103b04e5fcc8cf42ebf4134b2ef42
jenntang1 <1039@holbertonschool.com>
2020-05-15 06:22:23 +0000

    2. Updating to be in compliance with semistandard

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Updates the text color of the HTML tag HEADER to red and back to green
 
-$(function() {
-    $('DIV#toggle_header').click(function() {
-        $('header').toggleClass('red green')
-    });
+$(function () {
+  $('DIV#toggle_header').click(function () {
+    $('header').toggleClass('red green');
+  });
 });
--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // Updates text in header
 
-$(function() {
-    $('DIV#update_header').click(function() {
-        $('header').text('New Header!!!');
-    });
+$(function () {
+  $('DIV#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
 });
